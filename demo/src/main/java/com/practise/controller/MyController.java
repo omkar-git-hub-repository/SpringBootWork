3ab25af22c2f26b6292650e639d6b266fbb3293f
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-07-31 14:09:55 +0530

    performed getAllStudents Data

--- demo/src/main/java/com/practise/controller/MyController.java
+++ demo/src/main/java/com/practise/controller/MyController.java
@@ -1,10 +1,24 @@
 package com.practise.controller;
 
+import com.practise.entity.Students;
+import com.practise.repo.StudentRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MyController {
 
+        @GetMapping("/getAllStudents")
+        public ResponseEntity <List<Students>> getName(){
 
+            List<Students> studList1 = StudentRepo.SetAllEmployee();
+            return new ResponseEntity<>(studList1, HttpStatus.OK);
+
+        }
 
 }
