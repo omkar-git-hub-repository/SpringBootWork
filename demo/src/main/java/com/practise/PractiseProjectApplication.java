206c09a811b472077adf24367e9ce1f36edfabc1
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-07-31 13:38:53 +0530

    Created New Project

---
+++ demo/src/main/java/com/practise/PractiseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseProjectApplication.class, args);
+	}
+
+}
