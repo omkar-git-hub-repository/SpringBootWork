8a2ea337aafe7e88e628dcb93e83d3c026a3eea5
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-01 16:10:14 +0530

    Configured Matches Students Data

--- demo/src/main/java/com/practise/controller/MyController.java
+++ demo/src/main/java/com/practise/controller/MyController.java
@@ -4,10 +4,7 @@ import com.practise.entity.Students;
 import com.practise.repo.StudentRepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -84,7 +81,7 @@ public class MyController {
 
     // Add get response method
     @GetMapping("/getAlldata")
-    public ResponseEntity<List<Students>> getAllStudentsData(@RequestBody String fName){
+    public ResponseEntity<List<Students>> getAllStudentsData(@RequestParam("fName") String fName){
         List<Students> studentsList = StudentRepo.SetAllStudents();
         List<Students> matchedStudent = new ArrayList<>();
 
